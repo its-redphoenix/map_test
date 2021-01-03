dac0aa333747ad1784e6f19fb0f2e40f4322cf58
zephyrwala <mayur@redphoenix.co.in>
2021-01-03 18:05:21 +0530

    Map zoom function done

--- map_test/ViewController.swift
+++ map_test/ViewController.swift
@@ -6,12 +6,54 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    private var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.mapView.showsUserLocation = true
+        self.mapView.delegate = self
+        
+        self.locationManager.startUpdatingLocation()
+        setupUI()
+        
+        
+    }
+    
+    private func setupUI() {
+        let addFloadButton = UIButton(frame: CGRect.zero)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let coordinate = location.coordinate
+            
+            //this is used for zooming in the map
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01 )
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
